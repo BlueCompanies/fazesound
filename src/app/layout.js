d2779dcbc8269a003943a5a78ffca9c8863b5944
andres81 <andres_242001@hotmail.com>
2024-03-19 15:53:46 -0500

    google ads script added

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8002352274904314"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body>{children}</body>
     </html>
   );
